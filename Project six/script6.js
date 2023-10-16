ae818f0096ce62a185c172cc077934aaafe595c3
Manish Kumar <manish481181@gmail.com>
2023-10-17 03:39:51 +0530

    table

--- Project six/script6.js
+++ Project six/script6.js
@@ -7,7 +7,7 @@ window.addEventListener('keydown', function(e){
                 <th>Key</th><th>Key code</th><th>Code</th>
             </tr>
             <tr>
-                <td>${e.key == ' ' ? 'Space' : e.key}</td><td>${e.keyCode}</td><td>${e.code}</td>
+                <td>${e.key === ' ' ? 'Space' : e.key}</td><td>${e.keyCode}</td><td>${e.code}</td>
             </tr>
         </table>
         </div>`
